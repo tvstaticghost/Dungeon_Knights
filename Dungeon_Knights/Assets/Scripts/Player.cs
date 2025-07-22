80631128f859d6d61c52eadd4cc7849a215cdeea
tvstaticghost <ray.m.arnold@gmail.com>
2025-07-21 22:43:26 -0400

    Adding files

---
+++ Dungeon_Knights/Assets/Scripts/Player.cs
@@ -0,0 +1,82 @@
+using System.Threading;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] Animator animator;
+    [SerializeField] float movementSpeed;
+    private bool walking = false;
+
+    enum Direction { UP, DOWN, LEFT, RIGHT}
+    private Direction currentDirection = Direction.RIGHT;
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        Vector2 val = context.ReadValue<Vector2>();
+
+        if (val.x == 1.0)
+        {
+            walking = true;
+            currentDirection = Direction.RIGHT;
+            rb.linearVelocity = val * movementSpeed;
+        }
+        else if (val.x == -1.0)
+        {
+            walking = true;
+            currentDirection = Direction.LEFT;
+            rb.linearVelocity = val * movementSpeed;
+        }
+        else
+        {
+            walking = false;
+            rb.linearVelocity = new Vector2(0f, 0f) * movementSpeed;
+        }
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animator.Play("Paladin_Idle_Right");
+    }
+
+    private void PlayIdleAnimation()
+    {
+        switch (currentDirection)
+        {
+            case Direction.LEFT:
+                animator.Play("Paladin_Idle_Left");
+                break;
+            case Direction.RIGHT:
+                animator.Play("Paladin_Idle_Right");
+                break;
+        }
+    }
+
+    private void PlayWalkAnimation()
+    {
+        switch (currentDirection)
+        {
+            case Direction.LEFT:
+                animator.Play("Paladin_Walk_Left");
+                break;
+            case Direction.RIGHT:
+                animator.Play("Paladin_Walk_Right");
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (walking)
+        {
+            PlayWalkAnimation();
+        }
+        else
+        {
+            PlayIdleAnimation();
+        }
+    }
+}
