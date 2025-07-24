043d9a1eba9533c86d2ffc3e9648af8811715a1b
Sofia DiPaolo <ray.m.arnold@gmail.com>
2025-07-23 22:20:41 -0400

    Added more player animations and sprites

--- Dungeon_Knights/Assets/Scripts/Player.cs
+++ Dungeon_Knights/Assets/Scripts/Player.cs
@@ -31,6 +31,12 @@ public class Player : MonoBehaviour
             currentDirection = Direction.LEFT;
             rb.linearVelocity = val * movementSpeed;
         }
+        else if (val.y == 1.0)
+        {
+            walking = true;
+            currentDirection = Direction.UP;
+            rb.linearVelocity = val * movementSpeed;
+        }
         else
         {
             walking = false;
@@ -48,10 +54,13 @@ public class Player : MonoBehaviour
         switch (currentDirection)
         {
             case Direction.LEFT:
-                animator.Play("Paladin_Idle_Left");
+                animator.Play("Idle_West");
                 break;
             case Direction.RIGHT:
-                animator.Play("Paladin_Idle_Right");
+                animator.Play("Idle_East");
+                break;
+            case Direction.UP:
+                animator.Play("Idle_North");
                 break;
         }
     }
@@ -61,10 +70,13 @@ public class Player : MonoBehaviour
         switch (currentDirection)
         {
             case Direction.LEFT:
-                animator.Play("Paladin_Walk_Left");
+                animator.Play("Walk_West");
                 break;
             case Direction.RIGHT:
-                animator.Play("Paladin_Walk_Right");
+                animator.Play("Walk_East");
+                break;
+            case Direction.UP:
+                animator.Play("Walk_North");
                 break;
         }
     }
